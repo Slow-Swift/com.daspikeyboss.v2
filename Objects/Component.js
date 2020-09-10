13db5ade7ff1ce1248a12b67f137daec61142b65
DaSpikeyBoss <daspikeybos@tutanota.com>
2020-09-10 13:01:31 +0900

    jsDocked scripts in Objects. Also added .gitignore

--- Objects/Component.js
+++ Objects/Component.js
@@ -1,5 +1,19 @@
+/**
+ * @class Component
+ * 
+ * @author daspikeyboss
+ * 
+ * @classdesc The base class for components that can be added to Objects.
+ */
 class Component {
-  constructor(object, zOrder = 0.5) {
+
+  /**
+   * @constructor
+   * 
+   * @param {Object} object The object that the component will be attatched to. If missing a new Object will be created.
+   * @param {number} zOrder The order for script execution. Higher zOrders get executed first.
+   */
+  constructor(object=null, zOrder = 0.5) {
     if(!(object instanceof Object)) {
       object = new Object(object, Vector2.Zero, 0, Vector2.One);
     }
@@ -13,6 +27,11 @@ class Component {
     }
   }
 
+  /**
+   * Attatch this component to [object]
+   * 
+   * @param {Object} object The object to attatch this component to.
+   */
   AddToObject(object) {
     object.AddComponent(this);
     this.object = object;
@@ -25,32 +44,81 @@ class Component {
   }
 
 //functions to be overriden by component
+/**
+ * Called when the component is first added to a GameObject.
+ */
   Start() {}
 
+  /**
+   * Called each frame as long as the component is active.
+   */
   Update() {}
 
+  /**
+   * Called each frame after Update().
+   */
   LateUpdate() {}
 
+  /**
+   * Called when the Component is enabled.
+   */
   OnEnable() {}
 
+  /**
+   * Called when the Component is disabled.
+   */
   OnDisable() {}
 
+  /**
+   * Called when the component is detroyed.
+   */
   OnDestroy() {}
 
+  /**
+   * Called when the Object first enters into a collision with another Object.
+   * 
+   * @param {Collider2D} other The collider that was hit.
+   */
   OnCollisionEnter(other) {}
 
+  /**
+   * Called each frame that the Object is collided with another Object.
+   * 
+   * @param {Collider2D} other The collider that was hit.
+   */
   OnCollision(other) {}
 
+  /**
+   * Called when the Object exits a collision with another Object.
+   * 
+   * @param {Collider2D} other The collider that was hit.
+   */
   OnCollisionExit(other) {}
 
+  /**
+   * Called when the Mouse first collides with the Object.
+   */
   OnMouseOver() {}
 
+  /**
+   * Called when the Mouse exits a collision with the Object.
+   */
   OnMouseExit() {}
 
+  /**
+   * Called when the display changes size.
+   * 
+   * @param {Vector2} oldSize The size that the display used to be.
+   */
   OnDisplayResized(oldSize) {}
 
 //-------------//
 
+/**
+ * Activates / Deactivates the component based on [active].
+ * 
+ * @param {boolean} active Should the component be Activated (true) / Deactivate (false).
+ */
   SetActive(active) {
     this.wasActive = this._active;
     this._active = active;
@@ -65,10 +133,20 @@ class Component {
     }
   }
 
+  /**
+   * @returns whether the component is active or not.
+   */
   get active() {
     return this._active && this.object.active;
   }
 
+  //TODO: Decide what to do with SetActiveByObject().
+
+  /**
+   * Honestly Not Sure what this is.
+   * 
+   * @param {boolean} active 
+   */
   SetActiveByObject(active) {
     if(active) {
       if(this._active) {
@@ -81,23 +159,43 @@ class Component {
     }
   }
 
+  //TODO: See if there is a better way to do Require Component.
+
+  /**
+   * Make sure the Object has a component of type [type] attatched.
+   * 
+   * @param {Type} type The type of Component that is required.
+   * @param  {...any} args The arguments that should be used if creating a new component is required.
+   * 
+   * @returns the new component that has been created if there was no component of the required type originally.
+   *
+   */
   _RequireComponent(type, ...args) {
     if(this.object.FindComponentOfType(type) == null) {
       return new type(this.object, ...args);
     }
   }
 
+  /**
+   * Destroy this component.
+   */
   Destroy() {
     this._UnsubscribeFromUpdates();
     this.OnDestroy();
     this.object.RemoveComponent(this);
   }
 
+  /**
+   * Link the updates to main loop.
+   */
   _SubscribeToUpdates() {
     Loop.Subscribe(this.bndUpdate, false, this.zOrder);
     Loop.Subscribe(this.bndLateUpdate, true, this.zOrder);
   }
 
+  /**
+   * Unlink the updates from main loop.
+   */
   _UnsubscribeFromUpdates() {
     Loop.Unsubscribe(this.bndUpdate);
     Loop.Unsubscribe(this.bndLateUpdate, true);
--- Objects/Object.js
+++ Objects/Object.js
@@ -1,3 +1,13 @@
+/**
+ * @class Object
+ * 
+ * @author daspikeyboss
+ * 
+ * @classdesc A GameObject setup with a transform that can have other components added.
+ */
+
+ //TODO: Change The class to be called GameObject (iss 1).
+
 class Object {
   collisions = [];
   oldCollisions = [];
@@ -5,6 +15,16 @@ class Object {
   isMouseOver = false;
   wasMouseOver = false;
 
+  /**
+   * @constructor
+   * 
+   * Creates a new Object and registers it with ObjectManagement.
+   * 
+   * @param {string} name The name of the Object.
+   * @param {Vector2} position The position of the Object.
+   * @param {number} rotation The rotation of the Object.
+   * @param {Vector2} scale The scale of the Object.
+   */
   constructor(name, position, rotation, scale) {
     this.name = name;
     this.tag = "";
@@ -13,20 +33,38 @@ class Object {
     this.transform.object = this;
     this.active = true;
     this.bndUpdate = this.Update.bind(this);
-    this.SubAndUnsub();
+    this._SubAndUnsub();
     this.object = this;
     ObjectManagement.AddObject(this);
   }
 
+  /**
+   * Add [component] to the Object
+   * 
+   * @param {Component} component The componenet to be added to the Object.
+   */
   AddComponent(component) {
     this.components.push(component);
   }
 
+  /**
+   * Remove [component] from the Object.
+   * 
+   * @param {Component} component The component to be removed from the Object.
+   */
   RemoveComponent(component) {
     let index = this.components.indexOf(component);
     return ArrayTools.RemoveItem(this.components, index);
   }
 
+  /**
+   * Returns the component of type [type] if the Object has one attatched.
+   * 
+   * @param {Type} type The type of component that is needed.
+   * 
+   * @returns {Component | Null} the attatched component of type [type], null if there is no attatched 
+   * component of that type.
+   */
   FindComponentOfType(type) {
     for (let component of this.components) {
       if(component instanceof type) {
@@ -36,15 +74,23 @@ class Object {
     return null;
   }
 
+  /**
+   * Activates / Deactivates the Object bast on [active].
+   * 
+   * @param {boolean} active Should the Object be Activated (true) or Deactivated (false).
+   */
   SetActive(active) {
     this.active = active;
     for (let component of this.components) {
       component.SetActiveByObject(active);
     }
-    this.SubAndUnsub();
+    this._SubAndUnsub();
   }
 
-  SubAndUnsub() {
+  /**
+   * Subscribes and Unsubrbes the Update method on the Object based on the Objects active state.
+   */
+  _SubAndUnsub() {
     if(this.active) {
       Loop.Subscribe(this.bndUpdate, false, 1.1);
     } else {
@@ -52,10 +98,16 @@ class Object {
     }
   }
 
+  /**
+   * Called every frame that the Object is active.
+   */
   Update() {
     this.WorkCollisions();
   }
 
+  /**
+   * Check for collisions with other objects and call the related methods in all attatched Components.
+   */
   WorkCollisions() {
     for (let collision of this.collisions) {
       if(!this.oldCollisions.includes(collision)) {
@@ -96,6 +148,9 @@ class Object {
     this.oldCollisions = ArrayTools.CloneArray(this.collisions);
   }
 
+  /**
+   * Destroy the Game Object and remove it from the Scene.
+   */
   Destroy() {
     this.SetActive(false);
     let components = ArrayTools.CloneArray(this.components);
@@ -106,6 +161,11 @@ class Object {
     ObjectManagement.RemoveObject(this);
   }
 
+  /**
+   * When the screen is resized, call the related methods in all attatched components.
+   * 
+   * @param {Vector2} oldSize The size the screen used to be.
+   */
   OnDisplayResized(oldSize) {
     let components = ArrayTools.CloneArray(this.components);
     for (let component of components) {
