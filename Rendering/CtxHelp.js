9f6db12462c70077ae80c8e0d622ef2ea84e60a3
DaSpikeyBoss <daspikeybos@tutanota.com>
2020-09-10 10:59:31 +0900

    jsDocked CtxHelp

--- Rendering/CtxHelp.js
+++ Rendering/CtxHelp.js
@@ -1,5 +1,17 @@
+/**
+ * @class CtxHelp
+ * 
+ * @author daspikeyboss
+ * 
+ * @classdesc Provides some helper functions for working with the 2d HTML5 canvas context.
+ */
 class CtxHelp {
 
+  /**
+   * Transforms the canvas context to be in tandem with [transform2D].
+   * 
+   * @param {Transform2D} transform2D The transform that ctx needs to be aligned with.
+   */
   static TransformCtx(transform2D) {
     ctx.save();
     ctx.translate(transform2D.position.x, transform2D.position.y);
